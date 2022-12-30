c9bd650a3a88fd9a61603469a6902e0d19bac5e5
Robert Bauernfeind <70053333+robertbauernfeind@users.noreply.github.com>
2022-12-30 23:14:18 +0100

    initial commit

---
+++ todolist/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <div className="container pt-2">
+      <p className="h1">My ToDo-List</p>
+    </div>
+  )
+}
+
+export default App
