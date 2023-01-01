3dbf255513ece1add2e41a19b998a60659f6caab
Robert Bauernfeind <70053333+robertbauernfeind@users.noreply.github.com>
2023-01-01 14:56:25 +0100

    Update Bootstrap

--- testBootstrap/src/App.jsx
+++ testBootstrap/src/App.jsx
@@ -1,33 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <div className="container p-5 my-5">
+        <h1>My First Bootstrap Page</h1>
+        <p>This is some text</p>
+
+        <div className='row'>
+          <div className='col-sm p-3 bg-secondary text-white'>.col</div>
+          <div className='col-sm p-3 bg-dark text-white'>.col</div>
+          <div className='col-sm p-3 bg-secondary text-white'>.col</div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+
+    </>
   )
 }
 
