8b86f8129fd95c1654a1eb11433a545d39bfc0a1
Robert Bauernfeind <70053333+robertbauernfeind@users.noreply.github.com>
2023-01-07 12:30:12 +0100

    Test

--- testBootstrap/src/App.jsx
+++ testBootstrap/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MDEditor from "@uiw/react-md-editor"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const  [value, setValue] = useState('# Hello, world!')
 
   return (
     <>
       <div className="container p-5 my-5">
-        <h1>My First Bootstrap Page</h1>
+        <h1>Bootstrap test page</h1>
         <p>This is some text</p>
 
         <div className='row'>
@@ -15,6 +16,12 @@ function App() {
           <div className='col-sm p-3 bg-dark text-white'>.col</div>
           <div className='col-sm p-3 bg-secondary text-white'>.col</div>
         </div>
+
+        <MDEditor
+          value={value}
+          onChange={setValue}
+        />
+        <MDEditor.Markdown source={value} />
       </div>
 
     </>
